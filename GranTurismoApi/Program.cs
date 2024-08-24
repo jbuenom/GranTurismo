22a3f50bdfc907b7d1fea58eee7ff58f2c159f83
Javier Bueno <buenorz@gmail.com>
2024-08-24 12:50:04 +0200

    Add persistence model

--- GranTurismoApi/Program.cs
+++ GranTurismoApi/Program.cs
@@ -1,3 +1,6 @@
+using GranTurismoApi.GranTurismoApiDbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<GranTurismoDbContext>(
+                       options =>
+                       {
+                           var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                           if (!builder.Environment.IsDevelopment())
+                           {
+                               var password = Environment.GetEnvironmentVariable("MSSQL_SA_PASSWORD");
+                               connectionString = string.Format(connectionString!, password);
+                           }
+                           options.UseSqlServer(connectionString);
+
+                       });
 
 var app = builder.Build();
 
@@ -21,5 +37,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<GranTurismoDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
