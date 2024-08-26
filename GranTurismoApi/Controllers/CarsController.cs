56974cbc28659387190886dbf2c3b946742f9662
Javier Bueno <buenorz@gmail.com>
2024-08-26 20:50:36 +0200

    Add persist entities

--- GranTurismoApi/Controllers/CarsController.cs
+++ GranTurismoApi/Controllers/CarsController.cs
@@ -1,4 +1,6 @@
+using GranTurismoApi.GranTurismoApiDbContext;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GranTurismoApi.Controllers
 {
@@ -6,37 +8,77 @@ namespace GranTurismoApi.Controllers
     [Route("[controller]")]
     public class CarsController : ControllerBase
     {
-        private static readonly Car[] Cars =
-        [
-          new Car
-          {
-              Brand = "Ferrari",
-              Category = "Gr.3",
-              Model = "458 Italia",
-              Power = 570,
-              Weight = 1430
-          },
-          new Car
-          {
-              Brand = "Mercedes",
-              Category = "Gr.2",
-              Model = "C63 AMG",
-              Power = 510,
-              Weight = 1730
-          }
-        ];
-
+        private readonly GranTurismoDbContext _context;
         private readonly ILogger<CarsController> _logger;
 
-        public CarsController(ILogger<CarsController> logger)
+        public CarsController(ILogger<CarsController> logger, GranTurismoDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
-        public IEnumerable<Car> Get()
+        public async Task<ActionResult<List<Car>>> GetAllCars()
         {
-            return Cars;
+            var cars = await _context.Cars.ToListAsync();
+            return Ok(cars);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCar(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+            if (car is null)
+            {
+                return NotFound("Car not found");
+            }
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCar(Car car)
+        {
+
+            _context.Cars.Add(car);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateCar(Car updatedCar)
+        {
+            var dbCar = await _context.Cars.FindAsync(updatedCar.Id);
+            if (dbCar is null)
+            {
+                return NotFound("Car not found");
+            }
+
+            dbCar.Brand = updatedCar.Brand;
+            dbCar.Model = updatedCar.Model;
+            dbCar.Power = updatedCar.Power;
+            dbCar.Weight = updatedCar.Weight;
+            dbCar.Category = updatedCar.Category;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCar(int id)
+        {
+            var dbCar = await _context.Cars.FindAsync(id);
+            if (dbCar is null)
+            {
+                return NotFound("Car not found");
+            }
+
+            _context.Cars.Remove(dbCar);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
